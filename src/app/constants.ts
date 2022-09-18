f51e67a24d66c65b1db56a68d632ac4b78bd0bad
Kulasekaran <kula@htic.iitm.ac.in>
2022-09-18 16:52:35 +0000

    backend ip to 0.0.0.0

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
     //static url: string = "http://172.16.20.211:8000/";
-    static url: string = "http://13.212.187.115:8000/";
+    static url: string = "http://0.0.0.0:8000/";
     static posts: string = Constants.url + "posts/";
 }
